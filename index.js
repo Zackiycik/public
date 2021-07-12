c92fb30f0ff138f7b4c2a97813284a53623bbe8a
Zack <61936234+Zackiycik@users.noreply.github.com>
2021-07-12 22:03:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const mongoose = require("mongoose");
+mongoose.connect("Mongo URL", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true
+}).then(console.log("MongoDB bağlandı!")).catch(err => console.log(err))
 require("./BOTS/Database/index.js");
 require("./BOTS/Guard 1/index.js")
 require("./BOTS/Guard 2/index.js")
